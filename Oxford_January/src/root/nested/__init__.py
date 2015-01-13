4018fba2963cd18e766b9abc566b281971918d2c
mmmerlin <mmmerlin@ubuntu.(none)>
2015-01-13 16:19:55 +0000

    After Oxford day 1

--- Oxford_January/src/root/nested/__init__.py
+++ Oxford_January/src/root/nested/__init__.py
@@ -12,120 +12,82 @@ Make3DScatter = True
 MakeToFSpectrum = True
 AnalyseThresholdScan = False
 
-input_path = '/mnt/hgfs/VMShared/Data/chem_14.12.14/after lunch/c1_s2_01_thr400/'
+xmin = 0
+xmax = 230
+ymin = 0
+ymax = 230
 
+tmin = 28
+tmax = 28.6 
 
+nfiles_3d = 200
+
+def Make3DScatter():
+    xs, ys, ts = GetXYTarray_AllFilesInDir(input_path, xmin, xmax, ymin, ymax, -186.6, tmin, tmax, nfiles_3d)
+    print 'Number of X,Y,T points = %s'%len(xs)
+    
+    fig = pl.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(xs, ys, ts)
+    pl.xlim([0,200])
+    pl.ylim([0,200])
+    pl.show()
+    
+def MakeToFSpectrum():
+#         for file in os.listdir(input_path):
+    timecodes = GetTimecodes_AllFilesInDir(input_path, xmin, xmax, ymin, ymax, -186.6)
+    print 'Total entries = %s' %len(timecodes)
+    
+    min = 0
+    max = 50    
+    bins = (max-min)*50 -1
+    
+    #make the histogram of the timecodes
+    fig2 = pl.figure()
+    pl.subplot(2,1,1)
+    
+    pl.hist(timecodes, bins = bins, range = [min,max])
+    pl.ylim([0,75])
+    pl.xlim([min,max])
+    pl.title('Timepix SPectrum')
+ 
+ 
+    #plot PMT data
+    pmt_datafile = '/mnt/hgfs/VMShared/Data/oxford/PMTdata/005.txt'
+    data = pl.loadtxt(pmt_datafile)
+    pl.subplot(2,1,2)
+    pl.plot(data[:,0]*1e6 -84.3, -1.*data[:,1])
+    pl.ylim([0,20])
+    pl.xlim([min,max])
+    pl.title('PMT Spectrum')
+
+    
+    pl.show()
+
+def ShowCompositeImage(path):
+    
+    image = MakeCompositeImage_Medipix(path, xmin, xmax, ymin, ymax, maxfiles=99999)
+    
+    import lsst.afw.display.ds9 as ds9
+    try:
+        ds9.initDS9(False)
+    except ds9.Ds9Error:
+        print 'DS9 launch bug error thrown away (probably)'
+
+    ds9.mtv(image)
+    
 if __name__ == '__main__':
     print "Running mass spec analysis"
 
 
-     
-    #===========================================================================
-    # 3D Scattergram
-    #===========================================================================    
-    if Make3DScatter:
-        xs, ys, ts = GetXYTarray_AllFilesInDir(input_path, 1, 254, 1, 254, 0, 0, 250,10)
-        print 'Number of X,Y,T points = %s'%len(xs)
-        
-        mask = []
-        mask.append((2,3))
-        mask.append((12,13))
-        
-        for i in range(len(xs)):
-            print 'a'
-            if mask.index((xs[i],ys[i])) == -1:
-                xs[i],ys[i],ts[i] = -1,-1,-1
-        
-        fig = pl.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(xs, ys, ts)
-        pl.show()
+#     input_path = '/mnt/hgfs/VMShared/Data/oxford/Big run 1/'
+    input_path = '/mnt/hgfs/VMShared/Data/oxford/PMT Comp/'
 
-
-    #===========================================================================
-    # Histograms vs. PMT Spectra 
-    #===========================================================================
-    if MakeToFSpectrum:
-#         for file in os.listdir(input_path):
-        timecodes = GetTimecodes_AllFilesInDir(input_path, 3, 252, 3, 252, 2)
-        print 'Total entries = %s' %len(timecodes)
-        
-        #make the histogram of the timecodes
-        fig2 = pl.figure()
-        pl.subplot(2,1,1)
-        pl.hist(timecodes, bins = 601, range = [75,150])
-        pl.title('Oxygen - Timepix')
-     
-        #plot PMT data
-#         pmt_datafile = ''
-#         data = pl.loadtxt(pmt_datafile)
-#         pl.subplot(2,1,2)
-#         pl.plot(data[:,0], data[:,1])
-#         pl.title('Oxygen - PMT')
-
-        
-        pl.show()
-
-
-    #===========================================================================
-    # Compositing the phosphor image
-    #===========================================================================    
-    if False:
-        phosphor_map_path = '/mnt/hgfs/MShared/Data/Chem_09-06-14/Phosphor map data/'
-        
-        image = MakeCompositeImage_Medipix(phosphor_map_path, 2, 252, 2, 252, maxfiles=99999)
-        
-        import lsst.afw.display.ds9 as ds9
-        try:
-            ds9.initDS9(False)
-        except ds9.Ds9Error:
-            print 'DS9 launch bug error thrown away (probably)'
+#     Make3DScatter()
     
-        ds9.mtv(image)
+#     MakeToFSpectrum()
     
-    #===========================================================================
-    # Timecodes per frame as a function of threshold value
-    #===========================================================================
-    if AnalyseThresholdScan:
-        import string
-        from scipy.stats import norm
-        import matplotlib as mlab
-        threshold_path = 'mnt/hgfs/VMShared/Data/oxford/threshold_scan/'
-        
-        dirs = os.listdir(threshold_path)
-        
-        pl.figure()
-        
-        plotnum = 1
-        for dirname in dirs:
-            threshvalue = string.split(dirname, '_')[-1]
-#                if threshvalue!='320': continue
-    
-            histdata = []
-    
-            files = os.listdir(threshold_path + dirname)
-            for filename in files:
-                histdata.append(len(GetTimecodes_SingleFile(threshold_path + dirname + '/' + filename, 8, 247, 8, 247)))
-            
-#                (mu, sigma) = norm.fit(histdata)
-#                n, bins, patches = pl.hist(histdata, bins = 50, range = [0,250], facecolor='green', alpha=0.75)
-#                y = norm.pdf(bins, mu, sigma)
-#                l = pl.plot(bins, y, 'r--', linewidth=2)
-
-
-            pl.subplot(2,5,plotnum)
-            pl.title('Theshold = ' + str(threshvalue))
-            pl.hist(histdata, bins = 25, range = [0,250])
-            plotnum +=1
-
-    
-        
-        pl.tight_layout()
-        pl.show()
-
-
-        
-     
+#     TimepixDirToPImMMSDatafile('/mnt/hgfs/VMShared/Data/oxford/PMT comp/', '/mnt/hgfs/VMShared/Data/oxford/pimms.txt')
 
     print '\n***End code***'
 
