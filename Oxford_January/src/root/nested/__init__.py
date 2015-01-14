f559af3faf02ec821754da7b2a16497b67cfaa98
mmmerlin <mmmerlin@ubuntu.(none)>
2015-01-14 20:43:44 +0000

    Middle of day 3 Oxford 2

--- Oxford_January/src/root/nested/__init__.py
+++ Oxford_January/src/root/nested/__init__.py
@@ -8,10 +8,6 @@ from numpy import dtype
 from my_functions import *
 from matplotlib.pyplot import pause
 
-Make3DScatter = True
-MakeToFSpectrum = True
-AnalyseThresholdScan = False
-
 xmin = 0
 xmax = 230
 ymin = 0
@@ -35,37 +31,39 @@ def Make3DScatter():
     
 def MakeToFSpectrum():
 #         for file in os.listdir(input_path):
-#     timecodes = GetTimecodes_AllFilesInDir(input_path, xmin, xmax, ymin, ymax, -151.1)
-#     print 'Total entries = %s' %len(timecodes)
-#     
-#     min = 0
-#     max = 40    
-#     bins = (max-min)*50 -1
-#     
-#     #make the histogram of the timecodes
-#     fig2 = pl.figure()
-#     pl.subplot(2,1,1)
-#     
-#     pl.hist(timecodes, bins = bins, range = [min,max])
-# #     pl.ylim([0,75])
-#     pl.xlim([min,max])
-#     pl.title('Timepix SPectrum')
+    timecodes = GetTimecodes_AllFilesInDir(input_path, xmin, xmax, ymin, ymax, -151.1)
+    print 'Total entries = %s' %len(timecodes)
+     
+    tmin = 0
+    tmax = 40    
+    bins = (tmax-tmin)*50 -1
+     
+    #make the histogram of the timecodes
+    fig2 = pl.figure()
+    pl.subplot(2,1,1)
+     
+    pl.hist(timecodes, bins = bins, range = [tmin,tmax])
+#     pl.ylim([0,75])
+    pl.xlim([tmin,tmax])
+    pl.title('Timepix Spectrum')
  
- 
-    #plot PMT data
-#     pmt_datafile = '/mnt/hgfs/VMShared/Data/oxford/PMTdata/005.txt'
-    pmt_datafile = '/mnt/hgfs/VMShared/Data/oxford/Day 3/PMT/temp.csv'
-    
-    ReadTektronixWaveform(pmt_datafile)
-    exit()
-    
-    data = pl.loadtxt(pmt_datafile)
-    pl.subplot(2,1,2)
-    pl.plot(data[:,0]*1e6 -84.3, -1.*data[:,1])
-    pl.ylim([0,20])
-    pl.xlim([min,max])
-    pl.title('PMT Spectrum')
 
+#################################### 
+    #################################### plot PMT data
+#################################### 
+    pl.subplot(2,1,2)
+
+#     pmt_datafile = '/mnt/hgfs/VMShared/Data/oxford/Day 3/PMT/TEK0000.csv'
+#     xs, ys = ReadTektronixWaveform(pmt_datafile)
+#     pl.plot(xs*1e6 + 10, -1.*ys)
+
+    pmt_datafile = '/mnt/hgfs/VMShared/Data/oxford/PMTdata/005.txt'
+    xs, ys = ReadBNL_PMTWaveform(pmt_datafile)
+    pl.plot(xs*1e6 -84.3, -1.*ys)
+    
+#     pl.ylim([0,20])
+    pl.xlim([tmin,tmax])
+    pl.title('PMT Spectrum')
     
     pl.show()
 
