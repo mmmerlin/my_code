aac3d2b6e795a5dc02ebc8374a1dda3b9bd93279
mmmerlin <mmmerlin@ubuntu.(none)>
2015-03-29 13:07:29 -0700

    29/03/2015 1

--- Timepix_cluster_analysis/src/root/nested/__init__.py
+++ Timepix_cluster_analysis/src/root/nested/__init__.py
@@ -12,10 +12,11 @@ import lsst.afw.display.ds9 as ds9
 import lsst.afw.display.utils as displayUtils
 import lsst.afw.detection   as afwDetect
 from root_functions import ListToHist
+from lsst.afw.image import makeImageFromArray
 
 
-def TimepixToExposure_binary(filename, xmin, xmax, ymin, ymax, include_index):
-    from lsst.afw.image import makeImageFromArray
+def TimepixToExposure_binary(filename, xmin, xmax, ymin, ymax, mask_pixels):
+#     from lsst.afw.image import makeImageFromArray
 
     data = np.loadtxt(filename)
 
@@ -30,8 +31,9 @@ def TimepixToExposure_binary(filename, xmin, xmax, ymin, ymax, include_index):
         t = data[2]
         
         if x >= xmin and x <= xmax and y >= ymin and y <= ymax:
-            if include_index[y][x] == 0:
-                my_array[y,x] = 1
+#             if include_index[y][x] == 0:
+        
+            my_array[y,x] = 1
       
         my_image = makeImageFromArray(my_array)
     
@@ -42,8 +44,8 @@ def TimepixToExposure_binary(filename, xmin, xmax, ymin, ymax, include_index):
     
         for pointnum in range(len(x)):
             if x[pointnum] >= xmin and x[pointnum] <= xmax and y[pointnum] >= ymin and y[pointnum] <= ymax:
-                if include_index[y][x] == 0:
-                    my_array[y[pointnum],x[pointnum]] = 1
+#                 if include_index[y][x] == 0:
+                my_array[y[pointnum],x[pointnum]] = 1
             
         my_image = makeImageFromArray(my_array)
     
@@ -53,7 +55,15 @@ def TimepixToExposure_binary(filename, xmin, xmax, ymin, ymax, include_index):
     
     return my_image#[include_index]
 
-
+def MakeMaskArray(mask_list):
+    mask_array = np.ones((256,256), dtype = np.int32)
+    
+    for i in range(len(mask_list[0])):
+        y = mask_list[0][i]
+        x = mask_list[1][i]
+        mask_array[y][x] = 0
+    
+    return mask_array
 
 
 DISPLAY = True
@@ -81,40 +91,61 @@ if __name__ == '__main__':
 #     path     = '/mnt/hgfs/VMShared/Data/new_senors/7153-6(300nm)/Run1/'
 #     path     = '/mnt/hgfs/VMShared/Data/new_senors/7343-6(50nm_bad_bonds)/'
 #     path     = '/mnt/hgfs/VMShared/Data/new_senors/OLD_SENSOR_1/'
-    path     = '/mnt/hgfs/VMShared/Data/new_senors/OLD_SENSOR_2/Run3/'
+#     path     = '/mnt/hgfs/VMShared/Data/new_senors/OLD_SENSOR_2/Run3/'
 #     path = '/mnt/hgfs/VMShared/Data/new_senors/7153-6(300nm)/Run1/'
+
+    path     = '/mnt/hgfs/VMShared/Data/temp/temp/'
 
     
     #########################
     c1 = TCanvas( 'canvas', 'canvas', 500, 200, 700, 500 ) #create canvas
     
-    n_glitches = 0
-    n_goodframes = 0
-    n_pix_hits = 0
-    nfiles = 0.
-    
-    for filename in os.listdir(path):
-        xs, ys, ts = GetXYTarray_SingleFile(path + filename, xmin, xmax, ymin, ymax)
-        assert len(xs) == len(ys) == len(ts)
-        if len(xs) >= glitch_threshold: print "skipped glitch frame"; n_glitches +=1; continue
-        n_goodframes += 1
-        nfiles += 1
-        n_pix_hits += len(ts)
-         
-    pixels_per_frame = float(n_pix_hits) / float(n_goodframes)
+#     n_glitches = 0
+#     n_goodframes = 0
+#     n_pix_hits = 0
+#     nfiles = 0.
+#     
+#     for filename in os.listdir(path):
+#         xs, ys, ts = GetXYTarray_SingleFile(path + filename, xmin, xmax, ymin, ymax)
+#         assert len(xs) == len(ys) == len(ts)
+#         if len(xs) >= glitch_threshold: print "skipped glitch frame"; n_glitches +=1; continue
+#         n_goodframes += 1
+#         nfiles += 1
+#         n_pix_hits += len(ts)
+#          
+#     pixels_per_frame = float(n_pix_hits) / float(n_goodframes)
      
      
     intensity_array = MakeCompositeImage_Timepix(path, 1, 253, 1, 253, 0, 9999, -99999, 99999, return_raw_array=True)
-     
-    print 'nfiles = %s'%nfiles
+    nfiles = len(os.listdir(path))
+#     intensity_array /= float(nfiles)
+    mask_pixels = np.where(intensity_array >= 0.03*(nfiles))
+    print mask_pixels
+    for element in mask_pixels:
+        print element
+        
+    print mask_pixels
+    print intensity_array[187][19]
+    
+    mask = MakeMaskArray(mask_pixels)
+    intensity_array *= mask
+    
+    ds9.mtv(makeImageFromArray(intensity_array))
+    exit()
+    
+    
+    
       
     for thr_range in range(1,1001):
         badpixel_threshold = (float(thr_range)/1000.)*(nfiles)
         index = np.where(intensity_array <= badpixel_threshold)
         intensity_sum = intensity_array[index].sum(dtype = np.float64)
-        print str(thr_range/10.) + '\t' + str(intensity_sum/nfiles)
+#         print str(thr_range/10.) + '\t' + str(intensity_sum/nfiles)
      
-     
+    mask_pixels = np.where(intensity_array >= 0.03*(nfiles))
+    print 'done'
+    exit()
+#     index = np.where(intensity_array <= 0.03*(nfiles))
     index = np.where(intensity_array <= 0.03*(nfiles))
 
 
@@ -135,7 +166,7 @@ if __name__ == '__main__':
     
     display_num = 5
     for filenum, filename in enumerate(os.listdir(path)):
-        image = TimepixToExposure_binary(path + filename, xmin, xmax, ymin, ymax, include_index=index)
+        image = TimepixToExposure_binary(path + filename, xmin, xmax, ymin, ymax, mask_pixels=mask_pixels)
 
         if DISPLAY == True and filenum == display_num: ds9.mtv(image)
         
