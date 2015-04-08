a510521f0435157f31f9f2bf72abd4dd79058fd0
mmmerlin <mmmerlin@ubuntu.(none)>
2015-04-08 21:46:26 +0100

    08/04/2015 1

--- Timepix_cluster_centroiding/src/root/nested/__init__.py
+++ Timepix_cluster_centroiding/src/root/nested/__init__.py
@@ -15,6 +15,7 @@ from root_functions import ListToHist, ListVsList
 from lsst.afw.image import makeImageFromArray
 import matplotlib.pyplot as pl
 import cPickle as pickle
+from time import sleep
 
 from ROOT import *
 import ROOT
@@ -24,13 +25,22 @@ gROOT.SetBatch(1) #don't show drawing on the screen along the way
 
 DISPLAY = True
 
-PICKLED = False
+PICKLED = True
 
 path  = '/mnt/hgfs/VMShared/Data/new_sensors/bnl/24_03_2015/A2(300nm)/Run4/'
 #     path  = '/mnt/hgfs/VMShared/Data/Chem_09-06-14/Butanone_2us_delay/'
 OUTPUT_PATH = '/mnt/hgfs/VMShared/output/temp/'
-all_timecodes_pickle = OUTPUT_PATH + 'raw.pickle'
-centroided_timecodes_pickle = OUTPUT_PATH + 'p4.pickle'
+
+# all_timecodes_pickle = OUTPUT_PATH + 'raw.pickle'
+all_timecodes_pickle = OUTPUT_PATH + 'raw_claire.pickle'
+min_timecodes_pickle = OUTPUT_PATH + 'min.pickle'
+max_timecodes_pickle = OUTPUT_PATH + 'max.pickle'
+
+p2_timecodes_pickle = OUTPUT_PATH + 'p2.pickle'
+p4_timecodes_pickle = OUTPUT_PATH + 'p4.pickle'
+gaus_timecodes_pickle = OUTPUT_PATH + 'gaus.pickle'
+
+centroided_timecodes_pickle = OUTPUT_PATH + 'claire.pickle'
 
 
 xmin = 1
@@ -39,11 +49,16 @@ xmax = 254
 ymax = 254
 
 GLOBAL_OUT = []
-GLOBAL_OUT_2 = []
 
 OFFSET = 6400
 
 all_timecodes = []
+cluster_sizes = []
+min_timecode_list = []
+max_timecode_list = []
+p2_timecodes = []
+p4_timecodes = []
+gaus_timecodes = []
 centroided_timecodes = []
 
 
@@ -57,32 +72,23 @@ if __name__ == '__main__':
             except ds9.Ds9Error:
                 print 'DS9 launch bug error thrown away (probably)'
     
-
+#         OpenTimepixInDS9(path + '1_0002.txt')
+#         ds9.ds9Cmd('scale limits 6540 6580')
+#         
+#         exit()
         
-    #     OpenTimepixInDS9(path + '1_0002.txt')
-    #     exit()
-        
-        
-    #     thresholdValue = 10
-    #     npixMin = 1
-    #     grow = 0
-    #     isotropic = False
+        thresholdValue = 1
+        npixMin = 1
+        grow = 0
+        isotropic = False
     
-
         
         display_num = 0
         for filenum, filename in enumerate(os.listdir(path)):
+#             if filenum >= 10: continue
+#             if filenum != 0: continue
             if filenum%10==0: print 'Processing %s'%filenum
-    #         if filename != '1_0002.txt': continue
-#             if filenum >= 50: continue
     
-    
-            thresholdValue = 1
-            npixMin = 1
-            grow = 0
-            isotropic = False
-    
-    #         print filename
             image = TimepixToExposure(path + filename, xmin, xmax, ymin, ymax)
             if DISPLAY == True and filenum == display_num: ds9.mtv(image)
             
@@ -90,13 +96,14 @@ if __name__ == '__main__':
             footPrintSet = afwDetect.FootprintSet(image, threshold, npixMin)
             footPrintSet = afwDetect.FootprintSet(footPrintSet, grow, isotropic)
             footPrints = footPrintSet.getFootprints()
-            
-    #         print "Found %s footprints in %s"%(len(footPrints), filename)
+#             print "Found %s footprints in %s"%(len(footPrints), filename)
             
             footprint_pixels = 0
             for footprintnum, footprint in enumerate(footPrints):
-    #             if DISPLAY and filenum == display_num: displayUtils.drawBBox(footprint.getBBox(), borderWidth=0.5) # border to fully encompass the bbox and no more
+#                 if DISPLAY and filenum == display_num: displayUtils.drawBBox(footprint.getBBox(), borderWidth=0.5) # border to fully encompass the bbox and no more
                 npix = afwDetect.Footprint.getNpix(footprint)
+#                 if npix >1:cluster_sizes.append(npix)
+                
                 if npix >= 4:
                     box = footprint.getBBox()
                     bbox_xmin = box.getMinX()
@@ -106,51 +113,89 @@ if __name__ == '__main__':
                       
                     data = image.getArray()[bbox_ymin:bbox_ymax,bbox_xmin:bbox_xmax]        
                     centroid_x, centroid_y = footprint.getCentroid()
-                     
-    #                 x,y,t,chisq = CentroidTimepixCluster(data, OUTPUT_PATH + str(footprintnum) + '.png')
-                    x,y,t,chisq = CentroidTimepixCluster(data, fit_function = 'p4')
-                    x += xmin
-                    y += ymin
-                    if chisq < 1.2:
-                        centroided_timecodes.append(t-OFFSET)
-                    
-                    for value in GetAllTimecodesInCluster(data):
-                        all_timecodes.append(value-OFFSET)
+                    if abs((GetMaxClusterTimecode(data)-OFFSET - 160))<(2.5*5.5):
+                        x,y,t,chisq = CentroidTimepixCluster(data, fit_function = 'gaus')
+
+#                     x,y,t,chisq = CentroidTimepixCluster(data, fit_function = 'gaus')
+                        x += bbox_xmin
+                        y += bbox_ymin
+                        if chisq < 1.2:
+#                             x,y,t,chisq = CentroidTimepixCluster(data, OUTPUT_PATH + filename + str(footprintnum) + '_baaaaad.png', fit_function = 'gaus')
+                            centroided_timecodes.append(t-OFFSET)
+                        
+    #                     max_timecode_list.append(GetMaxClusterTimecode(data) - OFFSET)
+    #                     min_timecode_list.append(GetMinClusterTimecode(data) - OFFSET)
+                        
+                        for value in GetAllTimecodesInCluster(data):
+                            all_timecodes.append(value-OFFSET)
                  
-    #                 GLOBAL_OUT.append(str(x) + '\t' + str(y) + '\t' + str(t) + '\t' + str(chisq))
-    #                 GLOBAL_OUT_2.append(str(centroid_x) + '\t' + str(centroid_y))# + '\t' + str(image.getArray()[ymin,xmin] ))
                 ####next footprint
         ####next file
 
+     
+#         pickle.dump(min_timecode_list,      open(min_timecodes_pickle,          'wb'), pickle.HIGHEST_PROTOCOL)
+#         pickle.dump(max_timecode_list,      open(max_timecodes_pickle,          'wb'), pickle.HIGHEST_PROTOCOL)
         pickle.dump(all_timecodes,          open(all_timecodes_pickle,          'wb'), pickle.HIGHEST_PROTOCOL)
         pickle.dump(centroided_timecodes,   open(centroided_timecodes_pickle,   'wb'), pickle.HIGHEST_PROTOCOL)
+    
+    
+    
+    
+    
+    
     else:
         for item in pickle.load(open(all_timecodes_pickle, 'rb')):
             all_timecodes.append(item)
+        for item in pickle.load(open(min_timecodes_pickle, 'rb')):
+            min_timecode_list.append(item)
+        for item in pickle.load(open(max_timecodes_pickle, 'rb')):
+            max_timecode_list.append(item)
+
+        for item in pickle.load(open(p2_timecodes_pickle, 'rb')):
+            p2_timecodes.append(item)
+        for item in pickle.load(open(p4_timecodes_pickle, 'rb')):
+            p4_timecodes.append(item)
+        for item in pickle.load(open(gaus_timecodes_pickle, 'rb')):
+            gaus_timecodes.append(item)
+  
         for item in pickle.load(open(centroided_timecodes_pickle, 'rb')):
-            centroided_timecodes.append(item)
+            centroided_timecodes.append(item)   
+
     
 
 
+#     histmax = 60
+#     name = 'cluster_sizes'
+#     h1 = ListToHist(cluster_sizes, OUTPUT_PATH + 'cluster_sizes.png', log_y = False, nbins = (histmax-1), histmin = 0, histmax = histmax, name = name)
     
     
-    histmax = 400
-    name = 'raw_timecodes'
-    h1 = ListToHist(all_timecodes, OUTPUT_PATH + 'all_timecodes.png', log_y = True, nbins = (histmax-1), histmin = 1, histmax = histmax, name = name)
-      
-#     histmax = max(centroided_timecodes)
-    histmax = 400
-    name = 'centroided_timecodes'
-    h1 = ListToHist(centroided_timecodes, OUTPUT_PATH + 'centroided_timecodes_chi_sq_cut.png', log_y = True, nbins = (histmax-1), histmin = 1, histmax = histmax, name = name)
+    
+    
+    histmax = 600
+    threshold = 25
+    log = True
+#     name = 'min_timecode'
+#     h1 = ListToHist(min_timecode_list, OUTPUT_PATH + 'min_timecode.png', log_y = True, nbins = (histmax-1), histmin = 0, histmax = histmax, name = name, bang_a_gaus_through_it = True, fit_to_percentage_of_peak = threshold)
+    
+    name = 'max_timecode'
+    h2 = ListToHist(max_timecode_list, OUTPUT_PATH + 'max_timecode.png', log_y = log, nbins = (histmax)/2, histmin = 0, histmax = histmax, name = name, bang_a_gaus_through_it = True, fit_to_percentage_of_peak = threshold)
    
+    name = 'raw_timecodes'
+    h3 = ListToHist(all_timecodes, OUTPUT_PATH + 'all_timecodes.png', log_y = log, nbins = (histmax)/2, histmin = 0, histmax = histmax, name = name, bang_a_gaus_through_it = True, fit_to_percentage_of_peak = threshold)
+      
+    name = 'p2_timecodes'
+    h4 = ListToHist(p2_timecodes, OUTPUT_PATH + 'p2_timecodes.png', log_y = log, nbins = (histmax)/2, histmin = 0, histmax = histmax, name = name, bang_a_gaus_through_it = True, fit_to_percentage_of_peak = threshold)
+   
+    name = 'p4_timecodes'
+    h5 = ListToHist(p4_timecodes, OUTPUT_PATH + 'p4_timecodes.png', log_y = log, nbins = (histmax)/2, histmin = 0, histmax = histmax, name = name, bang_a_gaus_through_it = True, fit_to_percentage_of_peak = threshold)
+   
+    name = 'gaus_timecodes'
+    h6 = ListToHist(gaus_timecodes, OUTPUT_PATH + 'gaus_timecodes.png', log_y = log, nbins = (histmax)/2, histmin = 0, histmax = histmax, name = name, bang_a_gaus_through_it = True, fit_to_percentage_of_peak = threshold)
+  
+    name = 'claire_timecodes'
+    h6 = ListToHist(centroided_timecodes, OUTPUT_PATH + 'claire_timecodes.png', log_y = log, nbins = (histmax)/2, histmin = 0, histmax = histmax, name = name, bang_a_gaus_through_it = True, fit_to_percentage_of_peak = threshold)
+  
 
-#     for line in GLOBAL_OUT:
-#         print line
-#         
-#     print '--------------'
-#         
-#     for line in GLOBAL_OUT_2:
-#         print line
     
     print '\n***End code***'      
     
