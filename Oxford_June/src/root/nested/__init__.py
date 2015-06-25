dbf25004366dc1334d669edf0906f07f467abef7
user01 <merlin.fisherlevine@gmail.com>
2015-06-24 19:02:13 -0700

    Before making analysis simultaneous

--- Oxford_June/src/root/nested/__init__.py
+++ Oxford_June/src/root/nested/__init__.py
@@ -1,96 +1,139 @@
 import os
 from os.path import expanduser
 from mpl_toolkits.mplot3d import Axes3D
-
 import pylab as pl
 from numpy import dtype
-
 from my_functions import *
-
 from matplotlib.pyplot import pause
+from root_functions import *
+from TrackViewer import TrackToFile_ROOT_2D_3D
 
-xmin = 0
-xmax = 230
-ymin = 0
-ymax = 230
 
-tmin = 28
-tmax = 28.6 
 
-nfiles_3d = 200
 
-def Make3DScatter():
-    xs, ys, ts = GetXYTarray_AllFilesInDir(in_stem + path, xmin, xmax, ymin, ymax, -186.6, tmin, tmax, nfiles_3d)
-    print 'Number of X,Y,T points = %s'%len(xs)
+# exp_type = 'pimms'
+exp_type = 'timepix'
+
+day = 'day3'
+run = 'run8'
+
+
+
+if exp_type == 'timepix':
+    xmin = 10
+    xmax = 245
+    ymin = 10
+    ymax = 245
+#     tmin = 8060
+#     tmax = 8160
+    tmin = 9100
+    tmax = 9200
     
-    fig = pl.figure()
-    ax = fig.add_subplot(111, projection='3d')
+elif exp_type == 'pimms':
+    xmin = 0
+    xmax = 245
+    ymin = 0
+    ymax = 245
+    tmin = 1201
+    tmax = 1275
+else:
+    print 'INVALID EXP_TYPE'
+    exit()
+
+
+in_path = '/mnt/hgfs/VMShared/Data/oxford/june_2015/'
+in_path += day + '/'
+in_path += exp_type + '/'
+in_path += run + '/'
+
+
+out_path = '/mnt/hgfs/VMShared/output/oxford_june_2015/'
+
+
+conversion_in  = '/mnt/hgfs/VMShared/Data/oxford/june_2015/Day3/pimms/run8/'
+conversion_out = '/mnt/hgfs/VMShared/Data/oxford/june_2015/Day3/pimms_to_timepix/run8/'
+
+
+def PlotREMPI():
+    max_loops = 16
+    loop_length = 999
+    box_size = 1
     
-    import numpy as np
-    import my_image_tools
+    files = os.listdir(in_path)
+
+    wavelength = range(999)
+    intensity = np.zeros(999)
+    total_codes = 0
+    run_intensity = np.zeros(max_loops)
+
+    for i,filename in enumerate(files):
+        run_num = float(str.split(filename,'_')[0])
+        shot_num = float(str.split(str.split(filename,'_')[1],'.')[0])-1
+        if shot_num >= 999: continue
+
+        codes = GetRawTimecodes_SingleFile(in_path+filename, xmin, xmax, ymin, ymax, tmin, tmax, 0)
+        ncodes = len(codes[0:])
+        intensity[shot_num] += ncodes
+        total_codes += ncodes
+        run_intensity[run_num] += ncodes
+        
+        if i>=(max_loops*loop_length): break
+        
+    print 'Total codes = %s\n'%total_codes
+    for i in range(max_loops):
+        print 'run %s = %s'%(i,run_intensity[i])
+        
+    wavelength = wavelength[0:999-box_size+1]
+    intensity = BoxcarAverage1DArray(intensity,box_size)
+    assert len(wavelength)==len(intensity)
     
-    w = np.where(ts > 7143 and ts < 7158)
-    print w
-    ax.scatter(xs[w], ys[w], ts[w])
-    pl.xlim([0,200])
-    pl.ylim([0,200])
+    plotname = out_path + day + run + exp_type + 'REMPI.png'
+#     ListVsList(wavelength, intensity, out_path+plotname+'.png', plot_opt='AC')
+    
+    fig2 = pl.figure()
+    pl.plot(wavelength,intensity)
+    pl.xlabel('Wavelength (a.u./nm)', horizontalalignment = 'right' )
+    fig2.savefig(out_path+plotname+'.png')
+#     pl.show()
+    
+    fig3, ax = pl.subplots(nrows=1, ncols=1, sharex=True)
+    ax.errorbar(range(max_loops), run_intensity, run_intensity**0.5, fmt='o')
+    
+    tot_int = 0.
+    n_points = 0.
+    for i in range(len(run_intensity)):
+        if run_intensity[i] <> 0:
+            tot_int += run_intensity[i]
+            n_points += 1.
+    av_int = tot_int / n_points
+    
+    print 'average intensity = %.2f'%(av_int)
     pl.show()
     
+    
+    
+
 def MakeToFSpectrum():
-#         for file in os.listdir(input_path):
-#     timecodes = GetTimecodes_AllFilesInDir(in_stem + path, xmin, xmax, ymin, ymax, -???) #PEG?
-#     timecodes = GetTimecodes_AllFilesInDir(in_stem + path, xmin, xmax, ymin, ymax, -186.5) #Crystal violet
-#     timecodes = GetTimecodes_AllFilesInDir(in_stem + path, xmin, xmax, ymin, ymax, -155.1) #OCS
-    timecodes = GetTimecodes_AllFilesInDir(in_stem + path, 0, 255, 0, 255, -215) #OCS
+    timecodes = GetTimecodes_AllFilesInDir(in_path, xmin, xmax, ymin, ymax, 0) #OCS
     print 'Total entries = %s' %len(timecodes)
      
-#     tmin = 0
-#     tmax = 100
-#     bins = ((tmax-tmin) -1) *50
-      
-    tmin = 0
-    tmax = 4500
-    bins = (tmax-tmin) +1 
-     
     #make the histogram of the timecodes
-    fig2 = pl.figure()
-#     pl.subplot(2,1,1)
-     
-    pl.hist(timecodes, bins = bins, range = [tmin,tmax])
-#     pl.ylim([0,2000])
-    pl.xlim([tmin,tmax])
-#     pl.xlabel('Timecodes', horizontalalignment = 'right' )
-    pl.xlabel('ToF (us)', horizontalalignment = 'right' )
-    pl.title('Timepix ToF Spectrum')
-
-#################################### 
-    #################################### plot PMT data
-#################################### 
-#     pl.subplot(2,1,2)
-# # 
-#     pmt_datafile = '/mnt/hgfs/VMShared/Data/oxford/Day 3/PMT/TEK0004.csv'
-#     xs, ys = ReadTektronixWaveform(pmt_datafile)
-#     pl.plot(xs*1e6 + 10, -1.*ys)
-# # 
-# #     pmt_datafile = '/mnt/hgfs/VMShared/Data/oxford/Day 1/PMTdata/005.txt'
-# #     xs, ys = ReadBNL_PMTWaveform(pmt_datafile)
-# #     pl.plot(xs*1e6 -84.3, -1.*ys)
-# #     
-# #     pl.ylim([0,20])
+#     fig2 = pl.figure()
+#     pl.hist(timecodes, bins = bins, range = [tmin,tmax])
 #     pl.xlim([tmin,tmax])
-#     pl.title('PMT Spectrum')
-#     
-    fig2.savefig(out_stem + 'voltage/Full_ToF.png')
-#     pl.xlim([7100,7170])
-#     fig2.savefig(out_stem + path + 'ToF_ROI.png')
-# #     pl.xlim([6900,7170])
-#     fig2.savefig(out_stem + path + 'ToF_ROI_More.png')
-    
+#     pl.xlabel('ToF (timecodes)', horizontalalignment = 'right' )
+#     pl.title('ToF Spectrum')
+
+    f, (ax1, ax2) = pl.subplots(1, 2, sharey=False)
+    ax1.hist(timecodes, bins = 11811, range = [0,11810])
+    ax1.set_title('ToF Spectrum - Full range')
+    ax2.hist(timecodes, bins = (tmax-tmin) +1 , range = [tmin,tmax])
+    pl.xlim([tmin,tmax])
+    pl.xlabel('ToF (timecodes)', horizontalalignment = 'right' )
+    pl.title('ToF Spectrum - ROI')
+
+    f.savefig(out_path + day + run + exp_type + 'ToF.png')
     pl.show()
-    
-    
-      
-  
 
 def ShowCompositeImageMedipix(path):
     
@@ -104,90 +147,47 @@ def ShowCompositeImageMedipix(path):
 
     ds9.mtv(image)
     
-if __name__ == '__main__':
-    print "Running mass spec analysis"
-    from root_functions import *
-    from TrackViewer import TrackToFile_ROOT_2D_3D
+def ShowImage(image):
+    
+    import lsst.afw.display.ds9 as ds9
+    try:
+        ds9.initDS9(False)
+    except ds9.Ds9Error:
+        print 'DS9 launch bug error thrown away (probably)'
 
-    in_stem = '/mnt/hgfs/VMShared/Data/oxford_march_2015/E404_50nm_425/'
-    path = 'Voltage_scan/1000/'
-#     path = 'Day 2/Run 11/'
-    out_stem = '/mnt/hgfs/VMShared/output/oxford_march_2015/'
+    ds9.mtv(image)
     
-    in_stem = '/mnt/hgfs/VMShared/temp/'
-    path = 'burt/'
-    
-    from os import mkdir
-#     mkdir(out_stem + path)
-#     Make3DScatter()
+if __name__ == '__main__':
+    print "Oxford June 2015..."
+    print in_path
+
+
+    for i, filename in enumerate(os.listdir(conversion_in)):
+        TranslatePImMSToTimepix(conversion_in + filename, i, conversion_out)
+    print 'finished translating %s'%conversion_in
+    exit()
+
+
+    image = MakeCompositeImage_Timepix(in_path, maxfiles = 5000, t_min=tmin, t_max = tmax)
+    ShowImage(image)
+#     TrackToFile_ROOT_2D_3D(image.getArray(), out_path + 'comp.png', plot_opt='colz', log_z = False, force_aspect= True, fitline = None, zmax = 500)
+    exit()
+     
+     
+#     ShowCompositeImageMedipix(in_path)
 #     exit()
-#     TimepixDirToPImMMSDatafile('/mnt/hgfs/VMShared/Data/oxford/PMT comp/', '/mnt/hgfs/VMShared/Data/oxford/pimms.txt')
     
+#     in_path = in_path + '../pimms_to_timepix/'
+    
+#     PlotREMPI()
+#     exit()
+
+
 #     MakeToFSpectrum()
 #     print "Finished ToF Spectrum"
 #     exit()
+#     
     
-#     image = MakeCompositeImage_Timepix(in_stem + path, maxfiles = 2000, t_min=6960, t_max = 6980)
-#     image = MakeCompositeImage_Timepix(in_stem + path, maxfiles = 2000, t_min=3363, t_max = 3400)
-    image = MakeCompositeImage_Timepix(in_stem + path, maxfiles = 5000, t_min=3380, t_max = 3395)
-    TrackToFile_ROOT_2D_3D(image.getArray(), out_stem + '/voltage/all_timecodes.png', plot_opt='surf1', log_z = False, force_aspect= True, fitline = None, zmax = 10)
-#     import lsst.afw.display.ds9 as ds9
-#     try:
-#         ds9.initDS9(False)
-#     except ds9.Ds9Error:
-#         print 'DS9 launch bug error thrown away (probably)'
-   
-#     ds9.mtv(image)
-    exit()
-
-    
-    slicelist = []
-####### CO+ range
-    for i in range(7143,7158):
-        slicelist.append([i])
-#         slicelist.append([i,i+1])
-#         slicelist.append([i,i+2])
-    slicelist.append([7135,7139])#5 timecodes wide
-#     slicelist.append([7140,7144])#5 timecodes wide
-    slicelist.append([7145,7149])#5 timecodes wide
-    slicelist.append([7150,7154])#5 timecodes wide
-    slicelist.append([7155,7159])#5 timecodes wide
-    slicelist.append([7143,7158])# full sphere exactly
-    slicelist.append([7135,7166])# full sphere exactly + a bit more
-
-####### S+ range
-#     for i in range(7110,7136):
-#         slicelist.append([i])
-#     slicelist.append([7110,7114])#5 timecodes wide
-#     slicelist.append([7115,7119])#5 timecodes wide
-#     slicelist.append([7120,7124])#5 timecodes wide
-#     slicelist.append([7125,7129])#5 timecodes wide
-#     slicelist.append([7130,7134])#5 timecodes wide
-#     slicelist.append([7122,7133, '*'])#5 2nd peak integral S+
-#     slicelist.append([7114,7123, '*'])# 1st peak integral S+
-    
-
-#Run 2 S+ range
-#     for i in range(7123,7133):
-#         slicelist.append([i])
-#     slicelist.append([7123,7132])#5 whoel sphere
-#     slicelist.append([7129,7130])#5 peak
-    
-    
-    MakeTimeSlices(in_stem + path, slicelist, out_stem + path)
-    
-
-#     image = MakeCompositeImage_Timepix(in_stem + path, maxfiles = 9999, t_min=0, t_max = 11810)
-#     TrackToFile_ROOT_2D_3D(image.getArray(), out_stem + path + '1.png', plot_opt='surf1', log_z = False, force_aspect= True, fitline = None)
-
-#     import lsst.afw.display.ds9 as ds9
-#     try:
-#         ds9.initDS9(False)
-#     except ds9.Ds9Error:
-#         print 'DS9 launch bug error thrown away (probably)'
-#   
-#     ds9.mtv(image)
-
     print '\n***End code***'
 
 
