fcee65265b46cb98a2214442a512028b2497e9b3
user01 <merlin.fisherlevine@gmail.com>
2015-07-08 06:54:39 -0700

    Back from DESY

--- Oxford_June/src/root/nested/__init__.py
+++ Oxford_June/src/root/nested/__init__.py
@@ -9,55 +9,228 @@ from root_functions import *
 from TrackViewer import TrackToFile_ROOT_2D_3D
 
 
+GLOBAL_OUT = []
+
+exp_type = ''
+day = ''
+run = ''
+in_path = ''
+out_path = '/mnt/hgfs/VMShared/output/oxford_june_2015/temp/'
+
+in_path = '/mnt/hgfs/VMShared/Data/DESY/vacuum_gauge/'
 
 
-# exp_type = 'pimms'
-exp_type = 'timepix'
+# OpenTimepixInDS9( '/mnt/hgfs/VMShared/Data/oxford/Day 4/Run 20/1_0002.txt')
+# exit()
+
+# image = MakeCompositeImage_Medipix(in_path, 0, 255, 0, 255, 0, 99999)
+in_path = '/mnt/hgfs/VMShared/Data/oxford/Day 4/Run 20/'
+
+# image = MakeCompositeImage_Timepix('/mnt/hgfs/VMShared/Data/oxford/Day 4/Run 20/', 0, 255, 0, 255, 0, 3000)
+# 
+# import lsst.afw.display.ds9 as ds9
+# try:
+#     ds9.initDS9(False)
+# except ds9.Ds9Error:
+#     print 'DS9 launch bug error thrown away (probably)'
+# 
+# ds9.mtv(image)
+# exit()
+
+xmin, xmax, ymin, ymax,tmin,tmax=0,0,0,0,0,0
+
+
+def MakeToFSpectrum_new():
+    timecodes = GetTimecodes_AllFilesInDir(in_path, xmin, xmax, ymin, ymax, 0) #OCS
+    print 'Total entries = %s' %len(timecodes)
+     
+    if exp_type == 'timepix':
+        ax5.hist(timecodes, bins = 11811, range = [0,11810])
+        ax5.set_title('ToF Spectrum - Full range')
+        ax7.hist(timecodes, bins = (tmax-tmin) +1 , range = [tmin,tmax])
+        print 'Finished ToF / Timepix'
+#         pl.xlabel('ToF (timecodes)', horizontalalignment = 'right' )
+#         pl.title('ToF Spectrum - ROI')
+    elif exp_type =='pimms_to_timepix':
+        ax6.hist(timecodes, bins = 11811, range = [0,11810])
+        ax6.set_title('ToF Spectrum - Full range')
+        ax8.hist(timecodes, bins = (tmax-tmin) +1 , range = [tmin,tmax])
+        print 'Finished ToF / PImMS'
+
+
+def PlotREMPI_new():
+    max_loops = 16
+    loop_length = 999
+    box_size = 1
+    
+    files = os.listdir(in_path)
+
+    wavelength = range(999)
+    intensity = np.zeros(999)
+    total_codes = 0
+    run_intensity = np.zeros(max_loops)
+
+#     image_array = []
+#     for i in range(999):
+#         image = MakeCompositeImage_Timepix(in_path + files[0], xmin, xmax, ymin, ymax, t_min = tmin, t_max=tmax, return_raw_array=False)
+#         image_array.append(image)
+#         image_array[i] -= image
+        
+
+    for i,filename in enumerate(files):
+        if str(filename).find('.DS')!=-1:continue
+        run_num = float(str.split(filename,'_')[0])
+        shot_num = float(str.split(str.split(filename,'_')[1],'.')[0])-1
+        if shot_num >= 999: continue
+        
+        codes = GetRawTimecodes_SingleFile(in_path+filename, xmin, xmax, ymin, ymax, tmin, tmax, 0)
+        ncodes = len(codes[0:])
+        intensity[shot_num] += ncodes
+        total_codes += ncodes
+        run_intensity[run_num] += ncodes
+        
+        if i>=(max_loops*loop_length): break
+        
+    print 'Total codes = %s\n'%total_codes
+    for i in range(max_loops):
+        GLOBAL_OUT.append('run %s = %s'%(i,run_intensity[i]))
+        print 'run %s =\t%s'%(i,run_intensity[i])
+        
+    wavelength = wavelength[0:999-box_size+1]
+    intensity = BoxcarAverage1DArray(intensity,box_size)
+    assert len(wavelength)==len(intensity)
+    
+    if exp_type == 'timepix':
+        ax1.plot(wavelength,intensity)
+        ax1.set_xlabel('Wavelength (a.u./nm)')
+        ax3.errorbar(range(max_loops), run_intensity, run_intensity**0.5, fmt='o')
+        ax3.set_ylim([0, max(run_intensity)*1.1])
+        ax3.set_xlabel('Relative REMPI intensity (pixs/frame)')
+        
+        print 'Finished REMPI / Timepix'
+    elif exp_type =='pimms_to_timepix':
+        ax2.plot(wavelength,intensity)
+        ax2.set_xlabel('Wavelength (a.u./nm)')
+        ax4.errorbar(range(max_loops), run_intensity, run_intensity**0.5, fmt='o')
+        ax4.set_ylim([0, max(run_intensity)*1.1])
+        ax4.set_xlabel('Relative REMPI intensity (pixs/frame)')
+       
+        print 'Finished REMPI / PImMS'
+        
+    
+    tot_int = 0.
+    n_points = 0.
+    for i in range(len(run_intensity)):
+        if run_intensity[i] <> 0:
+            tot_int += run_intensity[i]
+            n_points += 1.
+    av_int = tot_int / n_points
+    
+    
+    print 'average intensity = %.2f'%(av_int)
+    GLOBAL_OUT.append('average intensity = %.2f'%(av_int))
+    
+    
+
+
+REMPI_fig, ((ax1, ax2), (ax3, ax4)) = pl.subplots(2, 2, sharey=False)
+TOF_fig, ((ax5, ax6), (ax7, ax8)) = pl.subplots(2, 2, sharey=False)
 
 day = 'day3'
 run = 'run8'
 
+# 
+# exp_type = 'timepix'
+# xmin = 10
+# xmax = 245
+# ymin = 10
+# ymax = 245
+# # tmin = 8060
+# # tmax = 8160
+# tmin = 6460
+# tmax = 6520
+# in_path = '/mnt/hgfs/VMShared/Data/oxford/june_2015/'
+# in_path += day + '/'
+# in_path += exp_type + '/'
+# in_path += run + '/'
+# 
+# GLOBAL_OUT.append('Timepix parameters')
+# GLOBAL_OUT.append('%s\t%s\t%s\t%s\t%s\t%s\n'%(xmin,xmax,ymin,ymax,tmin,tmax))
+#                   
+# PlotREMPI_new()
+# MakeToFSpectrum_new()
+# 
+# 
+# 
+# exp_type = 'pimms_to_timepix'
+# xmin = 0
+# xmax = 245
+# ymin = 0
+# ymax = 245
+# tmin = 1160
+# tmax = 1320
+# in_path = '/mnt/hgfs/VMShared/Data/oxford/june_2015/'
+# in_path += day + '/'
+# in_path += exp_type + '/'
+# in_path += run + '/'
+# 
+# GLOBAL_OUT.append('Timepix parameters')
+# GLOBAL_OUT.append('%s\t%s\t%s\t%s\t%s\t%s\n'%(xmin,xmax,ymin,ymax,tmin,tmax))
+# 
+# 
+# # PlotREMPI_new()
+# # MakeToFSpectrum_new()
+# 
+# REMPI_fig.set_size_inches(15,15)
+# REMPI_fig.savefig(out_path + day + run + 'REMPI.png')
+# TOF_fig.set_size_inches(15,15)
+# TOF_fig.savefig(  out_path + day + run + 'ToF.png')
+# 
+# 
+# outfile = open(out_path + day + run + 'data.txt', 'w')
+# for line in GLOBAL_OUT: outfile.write(line + '\n')
+# outfile.close()
+# 
+# print 'Finished combined analysis'
+# exit()
 
 
-if exp_type == 'timepix':
-    xmin = 10
-    xmax = 245
-    ymin = 10
-    ymax = 245
-#     tmin = 8060
-#     tmax = 8160
-    tmin = 9100
-    tmax = 9200
-    
-elif exp_type == 'pimms':
-    xmin = 0
-    xmax = 245
-    ymin = 0
-    ymax = 245
-    tmin = 1201
-    tmax = 1275
-else:
-    print 'INVALID EXP_TYPE'
-    exit()
 
 
-in_path = '/mnt/hgfs/VMShared/Data/oxford/june_2015/'
-in_path += day + '/'
-in_path += exp_type + '/'
-in_path += run + '/'
-
-
-out_path = '/mnt/hgfs/VMShared/output/oxford_june_2015/'
 
 
 conversion_in  = '/mnt/hgfs/VMShared/Data/oxford/june_2015/Day3/pimms/run8/'
 conversion_out = '/mnt/hgfs/VMShared/Data/oxford/june_2015/Day3/pimms_to_timepix/run8/'
 
 
+day = 'day3'
+run = 'run7'
+
+
+
+in_path = '/mnt/hgfs/VMShared/Data/oxford/june_2015/'
+exp_type = 'timepix'
+in_path += day + '/'
+in_path += exp_type + '/'
+in_path += run + '/'
+
+xmin = 10
+xmax = 245
+ymin = 10
+ymax = 245
+tmin = 8060
+tmax = 8160
+
+
+
+
+
+
+
 def PlotREMPI():
     max_loops = 16
     loop_length = 999
-    box_size = 1
+    box_size = 10
     
     files = os.listdir(in_path)
 
@@ -88,7 +261,8 @@ def PlotREMPI():
     assert len(wavelength)==len(intensity)
     
     plotname = out_path + day + run + exp_type + 'REMPI.png'
-#     ListVsList(wavelength, intensity, out_path+plotname+'.png', plot_opt='AC')
+    ListVsList(wavelength, intensity, plotname, plot_opt='AC')
+    exit()
     
     fig2 = pl.figure()
     pl.plot(wavelength,intensity)
@@ -111,7 +285,10 @@ def PlotREMPI():
     pl.show()
     
     
-    
+      
+ 
+
+
 
 def MakeToFSpectrum():
     timecodes = GetTimecodes_AllFilesInDir(in_path, xmin, xmax, ymin, ymax, 0) #OCS
@@ -162,25 +339,32 @@ if __name__ == '__main__':
     print in_path
 
 
-    for i, filename in enumerate(os.listdir(conversion_in)):
-        TranslatePImMSToTimepix(conversion_in + filename, i, conversion_out)
-    print 'finished translating %s'%conversion_in
-    exit()
+#     for i, filename in enumerate(os.listdir(conversion_in)):
+#         TranslatePImMSToTimepix(conversion_in + filename, i, conversion_out)
+#     print 'finished translating %s'%conversion_in
+#     exit()
 
 
-    image = MakeCompositeImage_Timepix(in_path, maxfiles = 5000, t_min=tmin, t_max = tmax)
-    ShowImage(image)
-#     TrackToFile_ROOT_2D_3D(image.getArray(), out_path + 'comp.png', plot_opt='colz', log_z = False, force_aspect= True, fitline = None, zmax = 500)
-    exit()
+#     image = MakeCompositeImage_Timepix(in_path, maxfiles = 5000, t_min=tmin, t_max = tmax)
+#     ShowImage(image)
+# #     TrackToFile_ROOT_2D_3D(image.getArray(), out_path + 'comp.png', plot_opt='colz', log_z = False, force_aspect= True, fitline = None, zmax = 500)
+#     exit()
+#      
+     
+     
+     
+     
+     
+     
+     
      
      
 #     ShowCompositeImageMedipix(in_path)
 #     exit()
     
-#     in_path = in_path + '../pimms_to_timepix/'
     
-#     PlotREMPI()
-#     exit()
+    PlotREMPI()
+    exit()
 
 
 #     MakeToFSpectrum()
